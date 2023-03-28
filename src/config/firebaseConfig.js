20219836065df1b26b2c4ca937c2019d642c0bda
Aayush7213 <105003666+Aayush7213@users.noreply.github.com>
2023-03-28 17:06:33 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAt-eHcM-kBZVUlhMDjymDEwEzTw-YKJPc",
+    authDomain: "cryptoxlorer.firebaseapp.com",
+    projectId: "cryptoxlorer",
+    storageBucket: "cryptoxlorer.appspot.com",
+    messagingSenderId: "23718907199",
+    appId: "1:23718907199:web:cd9de1bf650f157e1c178a"
+  };
+
+  export default firebaseConfig;
